4562d6eafb57148bea31ba4775d1dcee37d1aa28
Vitor Dias <vitordiasalves@gmail.com>
2025-08-05 16:49:09 -0300

    chore: conecta o servidor backend com o banco de dados

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mysql from "mysql";
 
 const app = express();
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"@Admin021867",
+    database:"test"
+});
+
+app.get("/", (req,res)=>{
+    res.json("Hello this is the backend");
+})
 
 app.listen(8800, ()=>{
-    console.log("Connected to backend");
+    console.log("Connected to backend!");
 })
