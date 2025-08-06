9206437a7dd4d34ef5a5e4cd706f9e6f908c6fa8
Vitor <vitordiasalves@gmail.com>
2025-08-06 17:58:13 -0300

    update: cria páginas iniciais e instala a biblioteca cors no backend

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function Books() {
-  return (
-    <div>Books</div>
-  )
+    const {books, setBooks} = useState([]);
+
+    useEffect(()=>{
+        const fetchAllBooks = async ()=>{
+            try{
+                const res = await axios.get("http://localhost8800/books");
+                console.log(res);
+            }catch(err){
+                console.log(err)
+            }
+        }
+        fetchAllBooks();
+    }, [])
+    return (
+        <div>Books</div>
+    )
 }
 
 export default Books
