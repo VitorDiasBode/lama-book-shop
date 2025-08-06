b62d108c62ea461970fc658979abfa9753980939
Vitor Dias <vitordiasalves@gmail.com>
2025-08-05 23:53:12 -0300

    feat: at: declara o endpoint POST para registrar novo livro

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mysql from "mysql";
 
 const app = express();
+app.use(express.json());
+
 const db = mysql.createConnection({
     host:"localhost",
     user:"root",
@@ -14,10 +16,24 @@ app.get("/", (req,res)=>{
 })
 
 app.get("/books", (req, res)=>{
-    const query = "SELECT * FROM books";
-    db.query(query, (err, data)=>{
+    const q = "SELECT * FROM books";
+    db.query(q, (err, data)=>{
         if(err) return res.json(err)
         return res.json(data)
+    })
+})
+
+app.post("/books", (req, res)=>{
+    const q = "INSERT INTO books (`title`, `description`, `cover`) VALUES (?)"
+    const values = [
+        req.body.title,
+        req.body.description,
+        req.body.cover,
+    ];
+
+    db.query(q,[values], (err, data)=>{
+        if(err) return res.json(req)
+        return res.json("Book has been created")
     })
 })
 
